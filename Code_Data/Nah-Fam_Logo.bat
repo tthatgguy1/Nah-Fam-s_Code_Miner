c4d9fb6395dafc901031402f616a5fa7b3e80b03
tthatgguy1 <68300078+tthatgguy1@users.noreply.github.com>
2024-12-14 15:34:04 -0600

    Nah-Fam_Logo.bat
    This is my company logo!

---
+++ Code_Data/Nah-Fam_Logo.bat
@@ -0,0 +1,118 @@
+:NAHFAM
+      @ECHO OFF
+      ECHO                                                                                  %COLOR_SLB_BRIGHTPURPLE%rr7r7i      7rrr7rL.      ri7rrr7:      :rrr7rL.   Yrr7r77%COLOR_RESET%            
+      PING localhost -n 1 >nul
+      ECHO                                                                                 %COLOR_SLB_BRIGHTPURPLE%g7iiii7i    .Qi:iii7:     Eriiiiii7.    .Qi:iiiri  rB:i:iiv.%COLOR_RESET%            
+      PING localhost -n 1 >nul
+      ECHO                                                                                %COLOR_SLB_BRIGHTPURPLE%:Q::iiii7:   iR:i:ii7.    J2::i:i:irr    iM.i:i:7:  7D::i:ir.%COLOR_RESET%            
+      PING localhost -n 1 >nul
+      ECHO                                                                                %COLOR_SLB_BRIGHTPURPLE%iQ:::iiii7   7E::iiir.    Di:i:i:i:ir.   rD::i:ir:  :g.::ii7%COLOR_RESET%             
+      PING localhost -n 1 >nul
+      ECHO                                                                                %COLOR_SLB_BRIGHTPURPLE%iM::i:iiirr  rg:::ii7.   vK:iiiii:ii7:   :R::iiir:  :g::iii7%COLOR_RESET%             
+      PING localhost -n 1 >nul
+      ECHO                                                                                %COLOR_SLB_BRIGHTPURPLE%iR::iiiiii7: rD::iir7.   Driii:P2:iir7   :g::iiir:  .Q:iiii7%COLOR_RESET%             
+      PING localhost -n 1 >nul
+      ECHO                                                                                %COLOR_SLB_BRIGHTPURPLE%ig::iiiiiirr :R:iiii7   ib::ii:vQ:iir7   :g:iiii7:  .Qi:iir7%COLOR_RESET%             
+      PING localhost -n 1 >nul
+      ECHO                                                                                %COLOR_SLB_BRIGHTPURPLE%iQ:iiririiir:rM::iir7   SJ:iir. Br:iiv:  :Qi:iirr:  .Q:iiir7%COLOR_RESET%             
+      PING localhost -n 1 >nul
+      ECHO                                      %COLOR_YELLOW%:75bBQBBBPu:%COLOR_RESET%                              %COLOR_SLB_BRIGHTPURPLE%:Qiiiiiiriiii7Z:iirrr   giiirr  Q1:iirr  .Biiiii7:  :Qi:iir7%COLOR_RESET%             
+      PING localhost -n 1 >nul
+      ECHO                                 %COLOR_YELLOW%.JRQBBBBBBBBBBBBBQBr%COLOR_RESET%                           %COLOR_SLB_BRIGHTPURPLE%.Qiiiriiiiirii7riri7i  vX:iiir..vL:iirv  .Qi:rirrr..:Jiiiirv%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                         %COLOR_YELLOW%iYi  YQBBBBBZPPPqdPPPPqbbgBBBY%COLOR_RESET%                         %COLOR_SLB_BRIGHTPURPLE%:Qiiriiijiiiriiirir7i  bv:riri7riiririY. .Biiirirr7rr:ririr7%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                      %COLOR_YELLOW%iBQBBBBBQBgdPPbgZbPdPbPbPbPdPPPBBB%COLOR_RESET%                        %COLOR_SLB_BRIGHTPURPLE%:Q:iiriig2:iiririrrv: .giririiiiiiirir7i  Br:riririiiiirirrv%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                      %COLOR_YELLOW%BBBBBDZPPPbPdPPqZMdPdPbPdPdPdPbqEBBr%COLOR_RESET%                      %COLOR_SLB_BRIGHTPURPLE%iRiirrr:LB7:rirrrrrv. 1Iiirir7I1u7iirr77  Q7iirrrrsLvriiri7v%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                    %COLOR_YELLOW%iBBBZPKRMbPdPdPbPbqEgPKPKPqPKbPdPdPPQBK%COLOR_RESET%                     %COLOR_SLB_BRIGHTPURPLE%rMirrrr7 sBr:ririrrL  Z7irrr7777LB7iirrv. gvirrrrvs7URirrrrL%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                  %COLOR_YELLOW%PBBBbXqKQQBPbPdPdPbPPXDRRQBBBQMbPPdPdPPgBR%COLOR_RESET%                    %COLOR_SLB_BRIGHTPURPLE%rgirrr7v  dQiirrrr7v .Mirrr7Y    Zq:rrr7i dvirrrL.  .Qrirr7v%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO               %COLOR_YELLOW%:BBBBPPqbPbPEqPPdPdPbqPDBBQbP5qXdZBgPqbPbPPEBQ%COLOR_RESET%                   %COLOR_SLB_BRIGHTPURPLE%rMirrrrs   QQiirrrrv Y5irrrvr    jQiirr7r bLirrrY:  .Qirrr7s%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO              %COLOR_YELLOW%BBBgDqPPbPdPbqbPbPdPbKgBBbSU1UI5IJJqBBbXbPdPPEBg%COLOR_RESET%                  %COLOR_SLB_BRIGHTPURPLE%7gir7r7J:   BQrirrv7 P7irr7Ji    :Brirrrv.ZYir7r1:  .BrirrvY%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO            %COLOR_YELLOW%LBQEPMqbPbPdPEPdPdPdPdXMQPuUUKX5U5IqPBQRBdKbPbPPZBU %COLOR_RESET%                %COLOR_SLB_BRIGHTPURPLE%rBs77LjX.    QB17s2: Bj77Y1I      BD7LL1u.EQ7vvUX.  .B57vYUU%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO           %COLOR_YELLOW%ZBQPqDZPPbPdPdbdPEPdPdqPBdj2IEIIXEEDgBQPPDBdqbPdqqBB%COLOR_RESET%                  %COLOR_SLB_BRIGHTPURPLE%PQKPqX.      JgD5:  KMdEqj.      rgZPPs  :ZdPP1:    JMqqP1%COLOR_RESET%             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO          %COLOR_YELLOW%DBDPPPdRqdPdPbPdPdPdPbPPDBBRPPQBRQgDPDqQQBMBQQQBBBQBB%COLOR_RESET%                                                                                         
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO         %COLOR_YELLOW%iBMPPdPbgEPbPdPdPdPbbdPdXgBBuiBMdbXgZdQBBBRE1BBQbKI7rB.%COLOR_RESET%                                                                                        
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO         %COLOR_YELLOW%BBPPdPdPEMbPdPdPEPEPdbdPPPBMZDbXDggBBBBPY.           JB%COLOR_RESET%                  %COLOR_SLB_PURPLE%:vssLsYJLsLsLJLJui    :vLYLs7.       .7sssv.       :uYLjJi%COLOR_RESET%            
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO         %COLOR_YELLOW%BBXPPdbdPgDPPbPdPdPbPbPdPqQB2ZMRBBBPr%COLOR_RESET%.    ....   .    MB    .           %COLOR_SLB_PURPLE%XP777v77r777r7777sJ  .ZJv77v7jJ      .ZL777Jj      :ML7v7v2.%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO         %COLOR_YELLOW%BBPPdPdPdqREPPdPbPbbdbdPdqPBBBBQX:%COLOR_RESET%    ..:......       rBDEBBB7          %COLOR_SLB_PURPLE%Burr7r7rrrrirrrrrvj  E2rrr7r7rji     Ibi7r77sv    .Mvr7rrrj:%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO         %COLOR_YELLOW%:BMPPdbEPbqREPPdPbbdPdPbPbqBQM.%COLOR_RESET%   ...:......     .7PQBQdgQQDBB          %COLOR_SLB_PURPLE%Qjirr7rUI5I5IS5KqP. :Rirrrrrr77J     DUir7r771:   P1i7r7r7sr%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO          %COLOR_YELLOW%gBPbPEPdPbPREbPdPdPdPdPZBBQ7%COLOR_RESET%  ...... ....  ::r1QQBMP5525SgDBv          %COLOR_SLB_PURPLE%Qvrr77v7YLJ7v77ri   Ejrr7rrr7r7j.    MUi7r7r77U  Ybi7r7r7rjr%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO           %COLOR_YELLOW%BBqbPdPbPPPMZPPdPdPPdBBQ:%COLOR_RESET%   .. .          XBBBEK2SSPPdPZEQB           %COLOR_SLB_PURPLE%Qsr7rvL            .Qrr7r7bJi77Jr    Q1r77r7r7Y7.Dr7r7r7r7Jr%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO           %COLOR_YELLOW%:BQKbbdPdPbPMZPPdPDBBg:%COLOR_RESET%   ...    :  .:iijQQgg5XKPPEdggRgQBBU          %COLOR_SLB_PURPLE%BLr777s            Sqr7rv:ZZir7vu    b2r777777v72L777777771r%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO            %COLOR_YELLOW%gBdqbPdPbPdqgEPdBQQ.%COLOR_RESET%         .rEBQBBBBBQRgMgbdZDMgRQBQBBBBB          %COLOR_SLB_PURPLE%QJr77vvi:iii:::i   B777vj :Brr7v1.   Z2rrv777v7v7777777v7vui%COLOR_RESET%          
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO             %COLOR_YELLOW%BBKqPPbqbqKSMBBu.%COLOR_RESET%       .7PQBBQEEbEggPQQRBBRQRQQBBBBBBBBBg          %COLOR_SLB_PURPLE%QYr777vY1uU1UuIPi 7gr77vv  BJr771r   EXr77777r77v7777777v7Ui%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO             %COLOR_YELLOW%7BQEPdPEdDgBBBB%COLOR_RESET%     .rXBBBRb5I2XXqqPMgQ  :BBBBBBBBBBQBBBB           %COLOR_SLB_PURPLE%QJrv7v7v7v7v7vvS: EUr77si  dU77vLU   qS77v77YYrv7vvvju7v7vUi%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO            %COLOR_YELLOW%2BMQQQMRDDDBQEPB.%COLOR_RESET%  iPBQBEK25XPqddZEgDQBB   .BBBBBBQBBBBY7B           %COLOR_SLB_PURPLE%Bs77v7vvJYJYuuXI  Q7v7vvYjJ7v7vvsI.  KP7v7L75gr7vvJ7d277vvIi%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO          %COLOR_YELLOW%.BBZPKqqq5qMBMP%COLOR_RESET%PEQq7BBQZQP5XbbZdDDMgQQBBBB. . .PJXgEPjr.   BB          %COLOR_SLB_PURPLE%Qu7vvLvSEDPZdDs  jb77vvvvYvv7v7vv5i  PK7vLvLrBE771u S57LvY2i%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO         %COLOR_YELLOW%SBBPPPPPPDBBBb%COLOR_RESET%PdgMQBBBBBBgMbDZggQRBBBBBBBBB  .. .r          EB          %COLOR_SLB_PURPLE%Bs7vYvU          g17LvYLjJjY77LvLuJ  XK7LvYJi BBQX  g177LLIr%COLOR_RESET%  
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO        %COLOR_YELLOW%BBgqbPPPQBBBB%COLOR_RESET%bBBBBBgQQ. :LBBBQQQBQBBBBBBBQB:  ... .2Y        iB         %COLOR_SLB_PURPLE%.BYvLvJ5         :Q7LvLYuXZPEgL7LvJ5  Z5vvLLUr  i:   R27LvY27%COLOR_RESET%          
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO       %COLOR_YELLOW%BBdqPPDBBQ5%COLOR_RESET%  QBQXPBBBgBE   .BBBQBBBBBQBQBBX   ...    Z: is5J:  Bv        %COLOR_SLB_PURPLE%.BvvvYsS         5PvvYLUv    BU7vYY5: bX7Lvs1s       QsvvYL57%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO      %COLOR_YELLOW%7BBQBBBBB7%COLOR_RESET%    BI .. .UBBBv    EBBBBBBQBBdr    r  ..  iYJBBBBBBj BM        %COLOR_SLB_PURPLE%.BLvYvJX         MUvLLJS:    Dgrvvsjs P57vYv11       QJvLvs2r%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO       %COLOR_YELLOW%gBBBEr%COLOR_RESET%      .B  iJI:..YBB.    :jgDSsi.       .::i:rbBBBP7PD MBZBB        %COLOR_SLB_PURPLE%.BvvvYsP.       .BvvvsJP     LBv7Yvu1 KX7LvYuK      .QLvvLYKr%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                    BB .L7:rs JBB.                :i72PEBBBQD2r.Bi :BQBB        %COLOR_SLB_PURPLE%.B27LvUE:       KB7vYLqY     .B27vYub.bRvvssqS      :BUvYY1E:%COLOR_RESET%           
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                     BE .7.:r7 PBB:  ..  . .  .5QBBBBBBBQE1r.  rB  .BQBq         %COLOR_SLB_PURPLE%BBQMQgs        UBQQRBK       ZBRRRDi iBQQRQP        QBMRQgj%COLOR_RESET%            
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                      BB:BU.i7v dBBv.   . .  qBBBDjvLri.    :SBBr  .BBB           %COLOR_SLB_PURPLE%....            ....          ...     .               .%COLOR_RESET%               
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                       BbBB.   : gBBBq.     7BBv:XBv:..:7PBBBBQ:   YBBB                                                                                
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                        ..PQBEgBBQBgQQBDv:i rQBD   LBBBBBBBPr      QBB:                                                                                 
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                            .r7: .BBQgRBBBBBdRDBBi    ...    :5: :QBBQ                                                                                  
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                   BBBggggQQBDggBBMr       rBBBBBBBQB                                                  .vSZRRMDX5uLi                    
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                    UBBQDgZggQZgDRBBBdYr:rUBBRgMgRBB:                                                uQQ27:...rJJSXQBB5                 
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                      MBBBBQgggZDDggQBBQBBBRgZgdZQBP                                               7BZ:     . :ii:i:r5BQ:              
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                        :SQBBBBBQBBBBBBBQQgQMBBBBB:                                              2BB.  ....... .v7rriirSBR     
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                            .B1vdDQBBBBBBBBQBBBQ%COLOR_BRIGHTCYAN%BBKbbUsr:.:.%COLOR_RESET%                                  .gBQi   ......... iii:i:i:iqB             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                          %COLOR_BRIGHTCYAN%iUPR%COLOR_RESET%          %COLOR_SLB_BRIGHTPURPLE%..:..%COLOR_RESET%     %COLOR_BRIGHTCYAN%iBBBBBBBBBBBQBdv.%COLOR_RESET%                           iBBd.   Y: ......   .i.::::::JB             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                     %COLOR_BRIGHTCYAN%.sgBBBQB:                  dQQRQQQQBQBBBBBBBQBIi%COLOR_RESET%                     5QBL      BQi ....  v21IPKdbbKSMB             
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                                 %COLOR_BRIGHTCYAN%iIBBBBBBBQQBBB5:            :UBBQMRMQMQRQMQRQRQQBBBBBBgv.%COLOR_RESET%             .MBBi      rBd.BX    rBr:::..    ..              
+      PING localhost -n 1 >nul                                                                                                                           
+      ECHO                             %COLOR_BRIGHTCYAN%rZBBBBBBQQQQRQMQQBBBBBQDPPKdDBBBBBBQRRMQRQRQMQgRMQRQMQQBBBBBBBBU:%COLOR_RESET%       rBBP.  ...  QBr   BBL. .BU                         
+      PING localhost -n 1 >nul                                                                                                                                                                                                                                                                                                                                                                                
+      ECHO.
+      ECHO.
+      ECHO.
+      ECHO.
+      ECHO %COLOR_BRIGHTWHITE%Press%COLOR_RESET% %COLOR_BRIGHTCYAN%any%COLOR_RESET% %COLOR_BRIGHTWHITE%key to continue.%COLOR_RESET%
+      ECHO.
+      ECHO.
+      ECHO.
+      ECHO.
+      timeout /T 6 >nul
